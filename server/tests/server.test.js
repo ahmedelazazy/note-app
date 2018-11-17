cad4245dc9e4c779d8390dcaf3fe7a506d663959
ahmedelazazy <ahmed.elazazy@hotmail.com>
2018-11-17 23:53:07 +0200

    add tests for DELETE users/me.token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -265,3 +265,23 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE users/me/token', () => {
+    it('should logout valid user', (done) => {
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', seedUsers[ 0 ].tokens[ 0 ].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                }
+                User.findById(seedUsers[ 0 ]._id)
+                    .then(user => {
+                        expect(user.tokens.length).toBe(0);
+                        done();
+                    }).catch(e => done(e));
+
+            });
+    });
+});
